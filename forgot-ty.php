2ed0b46c2149ae8ac181e5238d497ca09c923e1a
Joselin <joselin@seattlenewmedia.com>
2021-11-23 15:39:00 -0800

    ui changes for user login and password reset workflows.

--- forgot-ty.php
+++ forgot-ty.php
@@ -9,60 +9,66 @@ session_start();
 // }
 
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-  <link rel="shortcut icon" href="./img/favicon.png">
-
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
-  <link rel="stylesheet" type="text/css" href="./css/styles.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-  <script type="text/javascript" src="./js/login.js"></script>
-
-  <title>ESA Verification</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./img/favicon.png">
+    <title>ESA Verification</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
 
 <body>
-  <div class="container">
-    <div class="row"><div class="col-xs-4"></div><div class="col-xs-4">
-
-      <div class="heading">
-        <p class="heading-label">ESA Verification</p>
-        <img id="esa-logo" class="logo" src="./img/ESA_logo.PNG" alt="ESA logo" />
-      </div>
-
-    </div><div class="col-xs-4"></div></div>    
-
-  <div class="row"><div class="col-xs-2"></div><div class="col-xs-8">
-
-    <h4>Your password restore request was sent.</h4>
-    <p></p>
-    <h4>Please, check your email for further instructions. It may take up to a few minutes to show up.</h4>
-    <p></p>
-    <h4><b>Tip</b>: If you can't see email in Incoming folder - try to Refresh your email or search in Spam folder before requesting again.</h4>
-    
-  </div><div class="col-xs-2"></div></div>
-
+    <!-- BEGIN PAGE CONTENT -->
+    <main class="page-container">
+        <!-- Begin Logo Section -->
+        <section class="page-header-section">
+            <h1 class="page-heading">ESA Verification</h1>
+            <img id="esa-logo" class="logo" src="./img/ESA_logo.PNG" alt="ESA logo" />
+        </section>
+        <!-- End Logo Section -->
+        <!-- Begin Reset Message -->
+        <section class="content-center">
+          <h2 class="section-heading-small">Your password restore request was sent.</h4>
+          <p class="body-copy">Please, check your email for further instructions. It may take up to a few minutes to show up.</p>
+          <div class="snippet snippet-hint m-t-30">
+              <div class="snippet-body">
+                <p><span class="text-bold p-r-5">Tip:</span>If you can't see email in Incoming folder - try to Refresh your email or search in Spam folder before requesting again.</p>
+              </div>
+            </div>
+        </section>
+        <!-- End Reset Message -->
+    </main>
+    <!-- END PAGE CONTENT -->
+    <!-- BEGIN FOOTER -->
     <footer>
-      <!-- div>
-        <img id="federal-laws-logos" class="logos" src="./img/Federal_Laws_logos.PNG" alr="Federal Laws logos" />
-      </div -->
-      <span class="rights-text">© 2021 ESA Verification</span>
+        <span class="rights-text">© 2021 ESA Verification</span>
     </footer>
-  </div>
+    <!-- END FOOTER -->
+    <!-- BEGIN SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Page Specific Scripts -->
+    <script type="text/javascript" src="./js/login.js"></script>
+    <!-- END SCRIPTS -->
 </body>
 
 </html>
+ 
+
+
+
+
+
+
--- reset_err.php
+++ reset_err.php
@@ -2,57 +2,66 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-  <link rel="shortcut icon" href="./img/favicon.png">
-
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap">  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />    
-  <link rel="stylesheet" type="text/css" href="./css/styles.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
-  <title>ESA Verification</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./img/favicon.png">
+    <title>ESA Verification</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap">
+	<!-- Icons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />  
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
 
 <body>
-  
-  <div class="container">
-		<div class="row"><div class="col-xs-1"></div><div class="col-xs-10">
-
-			<div class="heading">
-			<p class="heading-label">ESA Verification</p>
-			<img id="esa-logo" class="logo" src="./img/ESA_logo.PNG" alt="ESA logo" />
+    <!-- BEGIN PAGE CONTENT -->
+    <main class="page-container">
+        <!-- Begin Logo Section -->
+        <section class="page-header-section">
+            <h1 class="page-heading">ESA Verification</h1>
+            <img id="esa-logo" class="logo" src="./img/ESA_logo.PNG" alt="ESA logo" />
+        </section>
+        <!-- End Logo Section -->
+		<!-- Begin Error Message -->
+		<section class="content-center">
+			<div class="snippet snippet-error">
+				<label class="snippet-heading">System Message</label>
+				<div class="snippet-body">
+					<?php echo $err_msg; ?>
+				</div>
 			</div>
-
-		</div><div class="col-xs-1"></div></div>    
-
-		<div class="row"><div class="col-xs-3"></div><div class="col-xs-6">
-			
-			<div style="text-align:left;">
-				<h4>System message:</h4>
-        <br>        
-        <div style="text-align:center;"><?php echo $err_msg; ?></div>
-      </div>
-    </div><div class="col-xs-3"></div></div>
-
-
-  </div>
-  <footer>
-	<!-- div>
-	  <img id="federal-laws-logos" class="logos" src="./img/Federal_Laws_logos.PNG" alr="Federal Laws logos" />
-	</div -->
-		<span class="rights-text">© 2021 ESA Verification</span>
-  </footer>
-
+		</section>
+		<!-- End Error Message -->
+    </main>
+    <!-- END PAGE CONTENT -->
+    <!-- BEGIN FOOTER -->
+    <footer>
+        <span class="rights-text">© 2021 ESA Verification</span>
+    </footer>
+    <!-- END FOOTER -->
+    <!-- BEGIN SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Page Specific Scripts -->
 	<script type="text/javascript" src="./js/reset_ok.js"></script>
+    <!-- END SCRIPTS -->
 </body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
