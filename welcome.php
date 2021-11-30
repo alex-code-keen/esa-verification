62719b331d0d235352634f4119d9bbc56c41f905
Joselin <joselin@seattlenewmedia.com>
2021-11-29 18:25:21 -0800

    esaverification page and welcome page ui

--- welcome.php
+++ welcome.php
@@ -22,84 +22,68 @@ if(!isset( $_SESSION["uuid"]) || $_SESSION["uuid"] !== true){
  
 <!DOCTYPE html>
 <html lang="en">
-<head>    
-	<title>Welcome</title>
+<head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">    
-	<!-- Optional theme -->
-	<!-- link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" -->
-	<!-- link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" -->
-	<!-- DataTables -->
-	<!--link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css"-->
-
-	<!--link rel="stylesheet" type="text/css" href="basic.css"-->
-	<link rel="stylesheet" type="text/css" href="./css/welcome.css">
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  
-
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="shortcut icon" href="./img/favicon.png">
+	<title>Welcome</title>
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,400;0,700;1,700&display=swap">
+	<!-- Styles -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/styles.css">
 </head>
+
 <body>
+	<!-- BEGIN NAVBAR -->
+	<header>
+		<nav class="navbar navbar-fixed-top dashboard-navbar">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">
+					<div class="navbar-logo-wrapper">
+						<img src="./img/ESA_logo.PNG" class="navbar-logo" alt="ESA"/> 
+					</div>
+					<span>Your ESA partner</span>
+				</a>
+			</div> 
+			<div class="navbar-menu-mobile"></div>       
+			<ul class="nav navbar-nav navbar-right">
+				<!-- <li class="active"><a href="">Home</a></li>
+				<li class=""><a href="">Messages</a></li> -->
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="">
+					<span class="glyphicon glyphicon-user"></span>  Profile <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="reset-password.php">Reset Password</a></li>
+					</ul>
+				</li>
+				<li class=""><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>  SignOut</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- END NAVBAR -->
 
-<nav class="navbar navbar-custom navbar-fixed-top">
-	<div class="container-fluid navbar-custom">            
-		<div class="navbar-custom navbar-header">
-			<a class="navbar-custom navbar-brand" href="">Your ESA partner</a>
-		</div>        
-		<ul class="nav navbar-custom navbar-nav">
-			<li class="navbar-custom active"><a href="">Home</a></li>
-			<!-- li class="navbar-custom"><a href="pets.php">Pets</a></li>                
-			<li class="navbar-custom dropdown">
-				<a class="navbar-custom dropdown-toggle" data-toggle="dropdown" href="">Documents
-				<span class="caret"></span></a>
-				<ul class="dropdown-menu">
-					<li><a href="">House</a></li>
-					<li><a href="">Travel</a></li>
-					<li><a href="airline.php">Airlines</a></li>
-					<li><a href="">Custom</a></li>
-				</ul>
-			</li>                
-			<li class="navbar-custom dropdown">
-				<a class="navbar-custom dropdown-toggle" data-toggle="dropdown" href="">Gallery
-				<span class="caret"></span></a>
-				<ul class="dropdown-menu">
-					<li><a href="gallery.php">View</a></li>
-					<li><a href="upload.php">Upload</a></li>
-				</ul>
-			</li--> 
-			<li class="navbar-custom"><a href="">Messages</a></li>
-		</ul>
-		<ul class="nav navbar-custom navbar-nav navbar-right">
-			<li class="navbar-custom dropdown">
-				<a class="navbar-custom dropdown-toggle" data-toggle="dropdown" href="">
-				<span class="glyphicon glyphicon-user"></span>  Profile <span class="caret"></span></a>
-				<ul class="dropdown-menu">
-					<!-- li><a href="">User info</a></li -->
-					<li><a href="reset-password.php">Reset Password</a></li>
-				</ul>
-			</li>
-			<li class="navbar-custom"><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>  SignOut</a></li>
-		</ul><br>
-	</div>
-</nav>
+	<!-- BEGIN DASHBOARD SECTION -->
+	<section class="dashboard-section">
+		<div class="container">
+		<h3>Welcome!</h3>
+		<p>.. more like excavation site here</p>
+		<p><?php echo 'userid -> '.$userid; ?></p>
+		<p><?php echo 'uuid -> '.$uuid; ?></p>
+		<a class="nav-link" href="index.html">Main ESA Verification (non-secure version)</a>
+		<a class="nav-link" href="landlordrequest.html">Landlord Request (non-secure version)</a>
+		<a class="nav-link" href="landlordrequestty.html">Landlord Request Thank You(non-secure version)</a>
+		</div>
+	</section>
+	<!-- END DASHBOARD SECTION -->
 
-<div class="container">
-  <h3>Any welcome message header</h3>
-  <p>.. more like excavation site here</p>
-  <p><?php echo 'userid -> '.$userid; ?></p>
-  <p><?php echo 'uuid -> '.$uuid; ?></p>
-	<br>
-	<a href="index.html">Main ESA Verification (non-secure version)</a><br>
-	<a href="landlordrequest.html">Landlord Request (non-secure version)</a><br>
-	<a href="landlordrequestty.html">Landlord Request Thank You(non-secure version)</a><br>
-</div>
-
-
-
+	<!-- BEGIN SCRIPTS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="./js/script.js"></script>
+	<!-- END SCRIPTS -->
 </body>
 </html>
