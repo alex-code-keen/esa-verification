1ff18ba9dd822c8bfe3c22e955add509c5e5ae16
Joselin <joselin@seattlenewmedia.com>
2021-12-02 10:38:05 -0800

    footer added

--- welcome.php
+++ welcome.php
@@ -79,6 +79,19 @@ if(!isset( $_SESSION["uuid"]) || $_SESSION["uuid"] !== true){
 	</section>
 	<!-- END DASHBOARD SECTION -->
 
+	<!-- BEGIN FOOTER -->
+	<footer class="dashboard-footer">
+		<p class="m-b-15">In Accordance with Federal Laws</p>
+		<img class="footer-logo m-b-15" src="./img/dashboard-footer-logo.JPG" alt="logo">
+		<p class="m-b-15">&copy; 2021 ESA Verification</p>
+		<ul class="list-inline footer-list">
+			<li><a href="#">Terms</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="#">Doctors</a></li>
+		</ul>
+	</footer>
+	<!-- END FOOTER -->
+	
 	<!-- BEGIN SCRIPTS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
