f57969f5c7ae9b86e94aad6fa0f463cd50242d5f
Joselin <joselin@seattlenewmedia.com>
2021-12-20 11:59:19 -0800

    removed scripts from 404 page footer

--- 404.php
+++ 404.php
@@ -30,13 +30,6 @@
           <span class="rights-text">© 2021 ESA Verification</span>
       </footer>
       <!-- END FOOTER -->
-      <!-- BEGIN SCRIPTS -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <!-- Page Specific Scripts -->
-      <script type="text/javascript" src="./js/login.js"></script>
-      <!-- END SCRIPTS -->
   </body>
 
 </html>
